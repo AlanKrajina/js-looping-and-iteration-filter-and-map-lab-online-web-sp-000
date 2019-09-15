ca79fe2ab67142734c1c57aacffbc90b92616eda
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 12:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers,revenue){
-
-
-
+  return drivers.filter(function(driver) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
 
 }
