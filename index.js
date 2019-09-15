a0630eefdda91069922f10790b031290e9953193
Alan Krajina <alankrajina88@gmail.com>
2019-09-15 13:02:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,3 @@ function driversWithRevenueOver(drivers,revenue){
     return driver.revenue > revenue;
   });
 }
-
-function driverNamesWithRevenueOver(drivers,revenue){
-  return drivers.filter(function(driver) {
-     return driver.revenue > revenue ) {
-
-       return driver.name;
-     }
-  });
-}
